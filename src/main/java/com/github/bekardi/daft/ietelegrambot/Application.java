229afdb1c26d472788e40e7af9084e1d147f6e32
Bekezhan <abdykarimovbekezhan@gmail.com>
2023-10-03 13:43:09 +0100

    DAFT_IE_TGB-0:
    added SpringBoot skeleton to the project.

---
+++ src/main/java/com/github/bekardi/daft/ietelegrambot/Application.java
@@ -0,0 +1,13 @@
+package com.github.bekardi.daft.ietelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
